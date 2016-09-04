9cb02944545478ae2e219b2c063286c57ee0aa0b
LaoLin <hi@laolin.com>
2016-09-04 11:40:53 +0800

    rename config file

--- module-wp/index.site-config.sample.php
+++ module-wp/index.site-config.sample.php
@@ -1,3 +1,5 @@
+<?php
+//把这个文件放在运行目录下，并改名为 index.site-config.php
 $mpcfg=[
 "APPID"=>"wx93301b9f5ddf5c8f",
 "SECRET"=>"",
@@ -5,3 +7,15 @@ $mpcfg=[
 "TOKEN" => "shqgs"
 ];
 $GLOBALS['mp']=$mpcfg;
+
+
+$GLOBALS['cfg']=[
+'WP_PATH'=>'./wordpress', // wp安装路径
+'WX_APPID'=>'',
+'WX_APPSECRET'=>''
+];
+
+define('WX_KEY','weixin_unionid');
+
+
+
